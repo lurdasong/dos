47f95b1617758bc1a0c4e6803da93564705851f8
lurdasong <lurdasong@gmail.com>
2022-04-30 20:14:01 +0300

    Finished UNO game v1.0

--- meniu.py
+++ meniu.py
@@ -1,28 +1,62 @@
 import os
 from uno import run_game
 
-print("---------------------------")
-print("-    Welcome to Dos       -")
-print("---------------------------")
-print("- 1 - Play game           -")
-print("- 2 - About the author    -")
-print("- 3 - The rules           -")
-print("- 4 - Quit                -")
-print("---------------------------")
-number = int(input("What do you want to do? "))
+def call():
+    meniu()
+    number = input("> ")
+    part(number)
 
-if number == 1:
+def meniu():
     os.system('clear')
-    run_game
+    print("---------------------------")
+    print("-    Welcome to Dos       -")
+    print("---------------------------")
+    print("- 1 - Play game           -")
+    print("- 2 - About the author    -")
+    print("- 3 - The rules           -")
+    print("- 4 - Quit                -")
+    print("---------------------------")
+   
 
-if number == 2:
-    os.system('clear')
-    print("Hi, it's me")
 
-if number == 3:
-    os.system('clear')
-    print("How do you not know how to play uno?")
 
-if number == 4:
-    os.system('clear')
-    quit()
+
+def part(number):
+    if number == "1":
+        os.system('clear')
+        input("Press enter to start game...")
+        #call()
+        run_game()
+    elif number == "2":
+        os.system('clear')
+        print("""
+        Hello, my name is Lurda Songailaite. I live in Lithuania. I study at LSMU gymansiun, II gynasium class A. 
+        This is my end of the year programming project, called 'Dos'. Why is it called like that? So i won't get copyright issues. 
+        Anyways, have fun!""")
+        input("Press enter to continue...")
+        call()
+    elif number == "3":
+        os.system('clear')
+        print("""
+        The game is really simple. First, do you know how to play 'Uno'? If you do, that's wonderful, because this game is really similar. 
+        Actually, it's the same, but i called it 'Dos', because i needed a new name. 
+        If you don't know how to play 'Uno', I'm going to quicly explain it to you. You start the game with seven cards and a table card in the middle. 
+        There are four colors of the cards - red, yellow, green and blue. Or in this game - R, Y, G, B. Also there are numbers on those cards - from 0 to 9.
+        There are also special cards. Skip (S) makes the other player skip their turn and draw 2 (D) makes the other player draw 2+ cards. There are also
+        wild cards. The normal wild card (WC) have the ability to change the table color and wild draw card (WD) can change color and make the other player draw 4+ cards. 
+        The game is played until one of the players has no cards left - that player wins. You can only discard cards that match the color or number or action (special card)
+        of the table card. If you don't have a card that you can play - you draw one from the deck. Pretty easy, isn't it? Now go try play it!
+        """)
+        input("Press enter to continue...")
+        call()
+    elif number == "4":
+        os.system('clear')
+        quit()
+    else:
+        os.system('clear')
+        print(f"What...? I don't understant what {number} means. Try again")
+        input("Press enter to continue...")
+        call()
+
+call()
+
--- uno.py
+++ uno.py
@@ -1,17 +1,20 @@
 import random
 import os
+from unittest import skip
+
+COLORS = ["R", "G", "B", "Y"]
 
 def generate_cards():
     cards = []
-    for c in ["R", "G", "B", "Y"]: # cards in all the colors
+    for c in COLORS : # cards in all the colors
         for x in range(1, 10): # cards from 1 to 9
             cards.append(f"{c}{x}") 
             cards.append(f"{c}{x}")
         cards.append(f"{c}0") # 4 zeroes
         cards.append(f"{c}S") # 4 skip
         cards.append(f"{c}S") # 4 skip
-        cards.append(f"{c}R") # 4 reverse
-        cards.append(f"{c}R") # 4 reverse
+        #cards.append(f"{c}R") # 4 reverse
+        #cards.append(f"{c}R") # 4 reverse
         cards.append(f"{c}D") # 4 draw 2
         cards.append(f"{c}D") # 4 draw 2
     #for x in range(4):
@@ -21,16 +24,20 @@ def generate_cards():
 
 
 def give_cards(cards):
-    player1 = cards[:7] # give player1 7 cards from the top
-    cards = cards[7:] # remove first 7 cards
-    player2 = cards[:7] # give player1 7 cards from the top
-    cards = cards[7:] # remove first 7 cards
-    table = cards[0] # take 1 cards from the top
-    cards = cards[1:] # remove 1 card from the top
-    return (table, player1, player2)
+    starting_card_count = 2
+    player1 = cards[:starting_card_count] # give player1 7 cards from the top
+    cards = cards[starting_card_count:] # remove first 7 cards
+    player2 = cards[:starting_card_count] # give player2 7 cards from the top
+    cards = cards[starting_card_count:] # remove first 7 cards
+    which_card_to_put_on_table = 0
+    while not cards[which_card_to_put_on_table][1].isnumeric():
+        which_card_to_put_on_table += 1
+    table = []
+    table.append(cards.pop(which_card_to_put_on_table)) # take 1 cards from the top and remove 1 card from the top
+    return (table, player1, player2, cards)
 
 # not used
-#def give_cards_n(cards, how_many_players):
+def give_cards_n(cards, how_many_players):
     players = []
     for i in range(how_many_players):
         player = cards[:7]
@@ -45,39 +52,194 @@ def give_cards(cards):
 def initialize_game():
     cards = generate_cards()
     random.shuffle(cards) # shuffle cards
-    table, player1, player2 = give_cards(cards) # gives cards to player1, player2 and table
-    return (table, player1, player2)
+    table, player1, player2, cards = give_cards(cards) # gives cards to player1, player2 and table
+    return (table, player1, player2, cards)
+
+def reshuffle_cards(table, cards):
+    while (len(table) > 1):
+        cards.append(table.pop(0))
+
+    # cards = table[:-1]
+    # table = table[-1:]
+    random.shuffle(cards)
+    return (table, cards)
+
+def turn_loop(state, player, which_player, cards): #act a turn
+    (table, new_color, skipped_turn, draw_count) = state
+    if table[-1][1] == "S" and not skipped_turn:
+        return ""
+    
+    while (True):
+        print("Table:", table[-1]) # prints the current table
+        if table[-1][0] == "W":
+            print(f"New color is {new_color}")
+        print(f"Player {which_player} - here are your cards:", player) # player is cards
+        player_played = input("Which card do you want to play? (D to draw instead) ")
+
+        if player_played.upper() == "D":
+            if draw_count == 0:
+                draw_count = 1
+            for n in range(draw_count):
+                if len(cards) == 0:
+                    print(f"Deck before: {cards}")
+                    reshuffle_cards(table, cards)
+                    print(f"Deck after: {cards}")
+                    print(f"Table: {table}")
+                if len(cards) == 0:
+                    # if table 1 card, deck 0 cards, all cards in hands, this crashes
+                    raise Exception("Players are retarded, game over.")
+                player.append(cards.pop())
+            draw_count = 0
+            return ""
+
+        if player_played not in player:
+            print("You do not have this card. Pick another one.")
+            continue
+        
+        if not can_card_be_played(state, player_played):
+            print("You cannot play this card. Pick another one.")
+            continue
+        else:
+            break
+
+    return player_played
+
+def color_change(player_played):
+    new_color = player_played[0]
+
+    if player_played[0] == "W":
+        new_color = input(f"Choose a color: {COLORS} ")
+        while new_color not in COLORS:
+            new_color = input(f"Wrong choice. Choose a color: {COLORS} ")
+    
+    return new_color
 
 
-def play_turn(table, player, which_player):
-    print("Table:", table) # prints the current table
-    print(f"Player {which_player} - here are your cards:", player) # player is cards
-    player_played = input("Which card do you want to play? ")
-    player.remove(player_played)
+def play_turn(state, player, which_player, cards):
+    (table, new_color, skipped_turn, draw_count) = state
     os.system('clear')
+    player_played = turn_loop(state, player, which_player, cards)
+    if player_played == '':
+        return (table, new_color, True, draw_count)
+    if player_played[1] == "D":
+        draw_count += 4 if player_played[0] == 'W' else 2
 
+    new_color = color_change(player_played)
+    
+    player.remove(player_played) # isimam is zaidejo rankos korta
+    table.append(player_played) # add card to top of table
+    return (table, new_color, False, draw_count)
+    
+    
+def can_card_be_played(state, played_card):
+    (table, new_color, skipped_turn, draw_count) = state
 
-def run_game_loop(table, player1, player2):
+    table_color = table[-1][0]
+    table_number = table[-1][1]
+    played_color = played_card[0]
+    played_number = played_card[1]
+
+    # on table:
+    # if card is skip
+        # check color
+    # if card is wild
+        # check color
+    # if card is draw 2
+        # if skipped_turn = False, can play only +2
+        # if skipped_turn = True, check color
+    # if card is draw 4
+        # if skipped_turn = False, can play only +4
+        # if skipped_turn = True, check color
+    # if card is normal
+        # check color
+        # check number
+
+    
+    if table[-1][1] == "S":
+        if played_card[0] == new_color or played_card[0] == "W":# color check
+            return True
+
+    if table[-1][0] == "W":
+        if played_card[0] == new_color or played_card[0] == "W":# color check
+            return True
+
+    if table[-1][1] == "D":
+        if skipped_turn:
+            if played_card[0] == new_color or played_card[0] == "W": # color check
+                return True
+        if not skipped_turn:
+            if table[-1][0] == "W":
+                if played_card == "WD":
+                    return True
+            else:
+                if played_card[0] != 'W' and played_card[1] == "D":
+                    return True
+
+    if (table[-1][0] in COLORS) and (table[-1][1].isnumeric()):
+        if table[-1][1] == played_card[1] or played_card[0] == "W": # number check
+            return True
+
+        if played_card[0] == new_color or played_card[0] == "W":# color check
+            return True
+
+    return False
+    
+
+def run_game_loop(table, player1, player2, cards):
+    players = [player1, player2]
+    new_color = table[-1][0]
+    skipped_turn = False
+    draw_count = 0
     while True:
-        play_turn(table, player1, 1)
-        play_turn(table, player2, 2)
-
+        for id, player in enumerate(players):
+            (table, new_color, skipped_turn, draw_count) = play_turn((table, new_color, skipped_turn, draw_count), player, id+1, cards)
+            if not player:
+                print(f"You won, player {id+1}")
+                return
+                
+                
 
 def run_game():
-    table, player1, player2 = initialize_game()
-    run_game_loop(table, player1, player2)
+    table, player1, player2, cards = initialize_game()
+    run_game_loop(table, player1, player2, cards)
+    
 
+if __name__ == "__main__":
+    run_game()
 
-#run_game()
 
 # the first card, as in table
 # check if player has the card he wants to play
-# game logics ( aloooot.....)
+# game logics:
 ## can card be played
-## special cards
-# add draw (2, 4, if you don't have)
+## skip
+## wild card
+## draw (2, 4, if you don't have)
 # the end
 
+# if (player has card he wants to play):
+    # run a loop to check if the player has any cards he can play
+        # if True
+            # if (card is playable):
+                #remove
+                # if card is skip
+                    # skips
+                # if card is wild
+                    # changes color
+                # if card is draw 2
+                    # adds 2 cards from 'cards'
+                # if card is draw 4
+                    # adds 4 cards from 'cards' and changes color
+            # else if it's not playable:
+                # tell him to pick another one
+        # else if False
+            # add 1 cards from 'cards'
+# else:
+    # print - player has no such card
+
+#if (player has no cards in his hand):
+    # player wins
+    # game closes (return to meniu?)
 
 
 
